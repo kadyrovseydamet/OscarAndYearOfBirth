c92856d77d0ab5cbfeaf31f8d6a95ce7a60e955a
kadyrovseydamet <kadyrovseydamet05@gmail.com>
2021-11-15 13:18:17 +0300

    В данном комите: Зафиксированно главное окно от маштабирования, улучшен дизайн, убраны некоторые недочеты в коде программы.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,15 +39,20 @@ namespace oscarandbirthday
             this.label5 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Calibri", 12F);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 3);
+            this.label1.Location = new System.Drawing.Point(-1, -3);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(560, 44);
+            this.label1.Size = new System.Drawing.Size(585, 70);
             this.label1.TabIndex = 0;
             this.label1.Text = "В этой программе сможете узнать, какой фильм получил премию Оскара в Ваш год рожд" +
     "ения.";
@@ -55,11 +60,12 @@ namespace oscarandbirthday
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold);
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.label2.Location = new System.Drawing.Point(48, 47);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-4, 3);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(489, 41);
+            this.label2.Size = new System.Drawing.Size(482, 41);
             this.label2.TabIndex = 1;
             this.label2.Text = "Внимание! Если в Ваш год рождения, никакой фильм не получал Оскара, то будут пока" +
     "заны результаты с крайней прошлой номинации!";
@@ -71,7 +77,7 @@ namespace oscarandbirthday
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Font = new System.Drawing.Font("Calibri", 12F);
-            this.textBox1.Location = new System.Drawing.Point(354, 131);
+            this.textBox1.Location = new System.Drawing.Point(354, 161);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(67, 27);
             this.textBox1.TabIndex = 2;
@@ -81,7 +87,7 @@ namespace oscarandbirthday
             // 
             this.label3.Font = new System.Drawing.Font("Calibri", 12F);
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(142, 132);
+            this.label3.Location = new System.Drawing.Point(142, 162);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(209, 25);
             this.label3.TabIndex = 3;
@@ -92,7 +98,7 @@ namespace oscarandbirthday
             // 
             this.label4.Font = new System.Drawing.Font("Calibri", 12F);
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(422, 132);
+            this.label4.Location = new System.Drawing.Point(422, 162);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 25);
             this.label4.TabIndex = 4;
@@ -101,13 +107,16 @@ namespace oscarandbirthday
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Indigo;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(250, 318);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(83, 31);
             this.button1.TabIndex = 5;
             this.button1.Text = "Узнать...";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label5
@@ -115,32 +124,35 @@ namespace oscarandbirthday
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.BackColor = System.Drawing.Color.Indigo;
             this.label5.Font = new System.Drawing.Font("Calibri", 12F);
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(16, 161);
+            this.label5.Location = new System.Drawing.Point(62, 212);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(556, 154);
+            this.label5.Size = new System.Drawing.Size(458, 73);
             this.label5.TabIndex = 6;
             this.label5.Text = "Тут будет показан ответ.";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Indigo;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.Location = new System.Drawing.Point(489, 318);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(83, 31);
             this.button2.TabIndex = 7;
             this.button2.Text = "Выход";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // linkLabel1
             // 
-            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Transparent;
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.BackColor = System.Drawing.Color.Red;
             this.linkLabel1.DisabledLinkColor = System.Drawing.Color.White;
             this.linkLabel1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.linkLabel1.LinkColor = System.Drawing.Color.White;
@@ -151,6 +163,25 @@ namespace oscarandbirthday
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Создал: kadyrovseydamet";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Indigo;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 65);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(54, 64);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(474, 45);
+            this.panel2.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -159,18 +190,21 @@ namespace oscarandbirthday
             this.ClientSize = new System.Drawing.Size(584, 361);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Oscar and year of birth";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,6 +221,8 @@ namespace oscarandbirthday
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
